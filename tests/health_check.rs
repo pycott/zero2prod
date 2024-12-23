2a20340460469f7ba09ef1dec430a67a31aa2a4f
Anton Skripnik <anstelli@gmail.com>
2024-12-23 11:00:21 +0800

    fmt fix

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,13 +1,13 @@
 use std::net::TcpListener;
 
 fn spawn_app() -> String {
-    let listener = TcpListener::bind("127.0.0.1:0")
-        .expect("Failed to bind random port");
+    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
     let port = listener.local_addr().unwrap().port();
     let server = zero2prod::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
     format!("http://127.0.0.1:{}", port)
 }
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app();
@@ -22,4 +22,3 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
-
